395072eef5e3d8a06a706cf08b5b8139e15217bc
tchen10 <tiffanychen10@gmail.com>
2015-02-11 16:09:17 -0600

    Factorial Tests - clean up

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,18 +2,21 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i == 1 || i == 0 ) {
+        if (i == 0 ) {
             return 1;
         }
-        if (i == 2) {
-            return 2;
-        }
-        if (i == 3) {
-            return 6;
+        if (i >= 1) {
+            int answer = 1;
+            for (int num = 0; num < i; num++) {
+                answer = answer * i;
+                i--;
+            }
+            return answer;
         }
         if (i < 0) {
             throw new IllegalArgumentException("Must be a positive number.");
         }
+
         return 0;
 
     }
