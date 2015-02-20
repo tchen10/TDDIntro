4825cca747a815ab6ba312e1a0307f0bd65931e1
tchen10 <tiffanychen10@gmail.com>
2015-02-20 12:45:52 -0600

    cleaned up factorial compute method

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,16 +2,12 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i == 0 ) {
+        if (i == 0 || i == 1 ) {
             return 1;
-        }
-        if (i >= 1) {
-            if (i == 1) return 1;
-            return i * compute(i-1);
         }
         if (i < 0) {
             throw new IllegalArgumentException("Must be a positive number.");
         }
-        return 0;
+        return i * compute(i-1);
     }
 }
